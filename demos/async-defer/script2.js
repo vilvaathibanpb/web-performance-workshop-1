8a8467dbc8a46108bfc993ad92e41b5a4a247076
Vilva Athiban <vilvaathiban@omios-MBP-8.fritz.box>
2021-03-06 02:49:58 +0100

    added demos

---
+++ demos/async-defer/script2.js
@@ -0,0 +1,5 @@
+let flag1 = true;
+
+setTimeout(() => (flag1 = false), 3000);
+// while (flag1 === true) {}
+console.log(2);
---
+++ demos/will-change/script1.js
@@ -0,0 +1,5 @@
+let flag = true;
+
+setTimeout(() => (flag = false), 3000);
+// while (flag === true) {}
+console.log(1);
